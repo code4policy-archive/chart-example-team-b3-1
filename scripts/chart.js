25e022453e34e81699850879e11088d8bad86c61
lmaharjan <58453420+lmaharjan@users.noreply.github.com>
2022-01-10 19:59:36 -0500

    updated the name of id which was missing a word

--- scripts/chart.js
+++ scripts/chart.js
@@ -29,7 +29,7 @@ var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .attr("class", "chart")
-    .attr("id", "apple-stock-chart")
+    .attr("id", "apple-stock-chart-container")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
